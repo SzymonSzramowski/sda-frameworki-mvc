26e55f79130aa7f7368742352e0bc9757be07971
Szymon Rybka <artifexet@gmail.com>
2017-08-06 23:08:07 +0200

    Created views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './views/home/home.component';
+import { ContactComponent } from './views/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
