b5b1d51f4943d3903c4cecb0daad14a5b57b15ad
Szymon Rybka <artifexet@gmail.com>
2017-08-08 00:14:27 +0200

    Form simplified

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -6,22 +6,14 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  @ViewChild('userNameInput')
-  public userNameInput: ElementRef;
-
-  @ViewChild('userPassowordInput')
-  public userPassowordInput: ElementRef;
-
   constructor() { }
 
   public ngOnInit() {
   }
 
-  public onLoginClick(event: MouseEvent): void {
+  public onLoginClick(event: MouseEvent, userName: string, userPassoword: string): void {
     event.preventDefault();
-    console.log('zaloguj', this.userNameInput.nativeElement.value,
-                      this.userPassowordInput.nativeElement.value);
+    console.log('zaloguj', userName, userPassoword);
   }
 
 }
